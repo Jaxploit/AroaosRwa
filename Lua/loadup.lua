a277174ac334595bb2131da03b209c9fd0e94ed8
JaxploitX <123088781+JaxploitX@users.noreply.github.com>
2023-10-10 15:39:49 +0200

    Create loadup.lua

---
+++ Lua/loadup.lua
@@ -0,0 +1,3 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/JaxploitX/AroaosRwaFalea/main/Orion')))()
+local Window = OrionLib:MakeWindow({Name = "Zero API Internal Executor", HidePremium = false, SaveConfig = true, ConfigFolder = "Zero API"})
+OrionLib:Destroy()
